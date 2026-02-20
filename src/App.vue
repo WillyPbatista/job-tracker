ba52b7384768bc7aa72e16a6e6b54546514049a8
WillyPbatista <willy012135@gmail.com>
2026-02-20 17:49:34 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div class="bg-white p-10 rounded-2xl shadow-xl">
+      <h1 class="text-4xl font-bold text-blue-600">
+        Tailwind funcionando 🚀
+      </h1>
+    </div>
+  </div>
+</template>
